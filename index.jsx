525327b3c654585db8f33ac3e54600f95d5fab4f
sumonaung23 <sumonaung575@gmail.com>
2023-09-15 21:30:52 +0630

    created components and added header.jsx and layout.jsx files inside it then moved the header section in index.jsx to header.jsx file then imported layout inside index.jsx

--- index.jsx
+++ index.jsx
@@ -5,24 +5,20 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetail from './pages/VanDetail';
+import Layout from './components/Layout';
 
 import "./server"
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path='/vans/:id' element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path='/vans/:id' element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
