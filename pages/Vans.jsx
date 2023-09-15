898c62b39197b3c9790e416c60cd108eb6c683d6
sumonaung23 <sumonaung575@gmail.com>
2023-09-15 10:23:25 +0630

    modified vans.jsx by wrapping Link tag inside the van-title div so that it can link to the detail page

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,16 +1,5 @@
 import React from "react"
-
-/**
- * {
-    * id: "1", 
-    * name: "Modest Explorer", 
-    * price: 60, 
-    * description: "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!", 
-    * imageUrl: "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png", 
-    * type: "simple"
- * }
- */
-
+import { Link } from 'react-router-dom'
 
 export default function Vans() {
     const [vans, setVans] = React.useState([])
@@ -22,12 +11,14 @@ export default function Vans() {
 
     const vanElements = vans.map(van => (
         <div key={van.id} className="van-tile">
-            <img src={van.imageUrl} />
-            <div className="van-info">
-                <h3>{van.name}</h3>
-                <p>${van.price}<span>/day</span></p>
-            </div>
-            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            <Link to={`/vans/${van.id}`}>
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
         </div>
     ))
 
