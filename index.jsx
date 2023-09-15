a2e8bfc10ea4a93e918dff79de2ec44aa6ea2e66
sumonaung23 <sumonaung575@gmail.com>
2023-09-16 00:16:06 +0630

    modified Route inside index.jsx

--- index.jsx
+++ index.jsx
@@ -17,16 +17,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path='/vans/:id' element={<VanDetail />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path='vans/:id' element={<VanDetail />} />
 
-          <Route path='/host' element={<HostLayout />} >
-            <Route path='/host/dashboard' element={<Dashboard />} />
-            <Route path='/host/income' element={<Income />} />
-            <Route path='/host/reviews' element={<Reviews />} />
+          <Route path='host' element={<HostLayout />} >
+            <Route index element={<Dashboard />} />
+            <Route path='income' element={<Income />} />
+            <Route path='reviews' element={<Reviews />} />
           </Route >
         </Route >
       </Routes>
