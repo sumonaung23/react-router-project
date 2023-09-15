17d2c448b02a7031be28f7aa018cae891f256257
sumonaung23 <sumonaung575@gmail.com>
2023-09-15 23:09:42 +0630

    modified index.css and added HostLayout.jsx file inside components folder and added nav and outlet inside it

--- index.jsx
+++ index.jsx
@@ -9,6 +9,7 @@ import Layout from './components/Layout';
 import Dashboard from './pages/Host/Dashboard';
 import Income from './pages/Host/Income';
 import Reviews from './pages/Host/Reviews';
+import HostLayout from './components/HostLayout';
 
 import "./server"
 
@@ -21,10 +22,13 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path='/vans/:id' element={<VanDetail />} />
-          <Route path='/host' element={<Dashboard />} />
-          <Route path='/host' element={<Income />} />
-          <Route path='/host' element={<Reviews />} />
-        </Route>
+
+          <Route path='/host' element={<HostLayout />} >
+            <Route path='/host/dashboard' element={<Dashboard />} />
+            <Route path='/host/income' element={<Income />} />
+            <Route path='/host/reviews' element={<Reviews />} />
+          </Route >
+        </Route >
       </Routes>
     </BrowserRouter>
   )
--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Dashboard = () => {
     return (
-        <h1>Dashboard page goes here.</h1>
+        <>
+            <h1>Dashboard page goes here.</h1>
+            <Outlet />
+        </>  
     )
 }
 
