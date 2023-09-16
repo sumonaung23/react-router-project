8ec63daaaa0160c3dd74ea1ca25e886b6ea6bd78
sumonaung23 <sumonaung575@gmail.com>
2023-09-16 21:54:54 +0630

    modified with param and fetch data using useEffect

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 
 const HostVanDetail = () => {
+    const param = useParams();
+    const [currentVan, setCurrentVan] = useState(null);
+
+    React.useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+            .then(res => res.json())
+            .then(data => setCurrentVan(data.vans));
+    }, [])
     return (
         <div>HostVanDetail</div>
     )
