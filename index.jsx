1e7a8ac979f550a3b6311134f35fb53d05cb2dde
sumonaung23 <sumonaung575@gmail.com>
2023-09-16 19:28:26 +0630

    modified by adding vansDetail and its route

--- index.jsx
+++ index.jsx
@@ -9,6 +9,8 @@ import Layout from './components/Layout';
 import Dashboard from './pages/Host/Dashboard';
 import Income from './pages/Host/Income';
 import Reviews from './pages/Host/Reviews';
+import HostVans from './pages/Host/HostVans';
+import HostVanDetail from './pages/Host/HostVanDetail';
 import HostLayout from './components/HostLayout';
 
 import "./server"
@@ -26,6 +28,8 @@ function App() {
           <Route path='host' element={<HostLayout />} >
             <Route index element={<Dashboard />} />
             <Route path='income' element={<Income />} />
+            <Route path='vans' element={<HostVans />} />
+            <Route path='vans/:id' element={<HostVanDetail />} />
             <Route path='reviews' element={<Reviews />} />
           </Route >
         </Route >
