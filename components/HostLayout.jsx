0c4236ecf2d45905a39c2bf3c880550cee0d12ee
sumonaung23 <sumonaung575@gmail.com>
2023-09-16 10:11:10 +0630

    modified index.css and HostLayout.jsx by adding Navlink and styling with conditional rendering

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, NavLink } from 'react-router-dom'
 
 const HostLayout = () => {
+    const activeNav = {
+        'font-weight': 'bold',
+        'text-decoration': 'underline',
+        'color': '#161616'
+    }
     return (
         <>
             <nav className="host-nav">
-                <Link to="/host">Dashboard</Link>
-                <Link to="/host/income">Income</Link>
-                <Link to="/host/reviews">Reviews</Link>
+                <NavLink 
+                    to="/host" className={activeNav}
+                    end
+                    style={({isActive}) => isActive ? activeNav : null}>                 
+                    Dashboard
+                </NavLink>
+                <NavLink 
+                    to="/host/income"
+                    style={({isActive}) => isActive ? activeNav : null}>
+                    Income
+                </NavLink>
+                <NavLink 
+                    to="/host/reviews"
+                    style={({isActive}) => isActive ? activeNav : null}>
+                        Reviews
+                </NavLink>
             </nav>
             <Outlet />
         </>
