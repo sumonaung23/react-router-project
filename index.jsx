53533c0c4b30df44ecbb9a7254c4664df11b4ecb
sumonaung23 <sumonaung575@gmail.com>
2023-09-16 23:15:20 +0630

    added styles for host-van-detail-layout-container

--- index.jsx
+++ index.jsx
@@ -11,6 +11,9 @@ import Income from './pages/Host/Income';
 import Reviews from './pages/Host/Reviews';
 import HostVans from './pages/Host/HostVans';
 import HostVanDetail from './pages/Host/HostVanDetail';
+import HostVanPhotos from './pages/Host/HostVanPhotos';
+import HostVanInfo from './pages/Host/HostVanInfo';
+import HostVanPricing from './pages/Host/HostVanPricing';
 import HostLayout from './components/HostLayout';
 
 import "./server"
@@ -28,9 +31,13 @@ function App() {
           <Route path='host' element={<HostLayout />} >
             <Route index element={<Dashboard />} />
             <Route path='income' element={<Income />} />
-            <Route path='vans' element={<HostVans />} />
-            <Route path='vans/:id' element={<HostVanDetail />} />
             <Route path='reviews' element={<Reviews />} />
+            <Route path='vans' element={<HostVans />} />
+            <Route path='vans/:id' element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path='pricing' element={<HostVanPricing />} />
+              <Route path='photos' element={<HostVanPhotos />} />
+            </Route>
           </Route >
         </Route >
       </Routes>
--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -1,17 +1,45 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { Link, useParams, Outlet } from 'react-router-dom';
 
 const HostVanDetail = () => {
-    const param = useParams();
+    const {id} = useParams();
     const [currentVan, setCurrentVan] = useState(null);
 
     React.useEffect(() => {
         fetch(`/api/host/vans/${id}`)
             .then(res => res.json())
             .then(data => setCurrentVan(data.vans));
-    }, [])
+    }, []);
+
+    if(!currentVan) {
+        return <h1>Loading...</h1>
+    }
+
     return (
-        <div>HostVanDetail</div>
+        <section>
+            <Link 
+                to=".."
+                relative='path'
+                className='back-button'
+                >&larr; <span>Back to all vans</span>
+            </Link>
+
+            <div className="host-van-detail-layout-container">
+                <div className="host-van-detail">
+                    <img src={currentVan.imageUrl} />
+                    <div className="host-van-detail-info-text">
+                        <i
+                            className={`van-type van-type-${currentVan.type}`}
+                        >
+                            {currentVan.type}
+                        </i>
+                        <h3>{currentVan.name}</h3>
+                        <h4>${currentVan.price}/day</h4>
+                    </div>
+                </div>
+                <Outlet />
+            </div>
+        </section>
     )
 }
 
