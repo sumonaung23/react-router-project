bd5976b431294ce2b0a8081b508564e0de4cd3a7
sumonaung23 <sumonaung575@gmail.com>
2023-09-18 17:49:42 +0630

    ommitted this as it's already inside the vans context

--- pages/Van/Vans.jsx
+++ pages/Van/Vans.jsx
@@ -19,7 +19,7 @@ export default function Vans() {
 
     const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
-            <Link to={`/vans/${van.id}`}>
+            <Link to={van.id}>
                 <img src={van.imageUrl} />
                 <div className="van-info">
                     <h3>{van.name}</h3>
