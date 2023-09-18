0cbe884051439cbb11b173df6f0b530475b3009e
sumonaung23 <sumonaung575@gmail.com>
2023-09-18 17:37:06 +0630

    added hadleFilterChange function and modified it in the eventHandler and made the clear button with condertional rendering

--- pages/Van/Vans.jsx
+++ pages/Van/Vans.jsx
@@ -30,9 +30,40 @@ export default function Vans() {
         </div>
     ))
 
+    function handleFilterChange(key, value) {
+        setSearchParams(prevParams => {
+            if (value === null) {
+                prevParams.delete(key)
+            } else {
+                prevParams.set(key, value)
+            }
+            return prevParams
+        })
+    }
+
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
+            <div className="van-list-filter-buttons">
+                <button 
+                    onClick={() => handleFilterChange("type", "simple")}
+                    className="van-type simple"
+                >Simple</button>
+                <button 
+                    onClick={() => handleFilterChange("type", "luxury")}
+                    className="van-type luxury"
+                >Luxury</button>
+                <button 
+                    onClick={() => handleFilterChange("type", "rugged")}
+                    className="van-type rugged"
+                >Rugged</button>
+                { typeFilter ?
+                    (<button 
+                    onClick={() => handleFilterChange("type", null)}
+                    className="van-type clear-filters"
+                    >Clear filter</button>) : null }
+            
+            </div>
             <div className="van-list">
                 {vanElements}
             </div>
