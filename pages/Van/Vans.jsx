e20adc5582bd9eab9d2005cb91944d6f5972054e
sumonaung23 <sumonaung575@gmail.com>
2023-09-19 10:00:49 +0630

    modified the state with search and type to get the specific van type for good UI

--- pages/Van/Vans.jsx
+++ pages/Van/Vans.jsx
@@ -19,7 +19,7 @@ export default function Vans() {
 
     const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
-            <Link to={van.id}>
+            <Link to={van.id} state={{ search: `?${searchParams.toString()}`, type: typeFilter }}>
                 <img src={van.imageUrl} />
                 <div className="van-info">
                     <h3>{van.name}</h3>
