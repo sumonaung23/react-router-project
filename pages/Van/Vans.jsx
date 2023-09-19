59a2b5ee7f57d5ef396ad136ed8789231e24307f
sumonaung23 <sumonaung575@gmail.com>
2023-09-19 11:14:20 +0630

    created for 404 page

--- pages/Van/Vans.jsx
+++ pages/Van/Vans.jsx
@@ -1,5 +1,6 @@
 import React from "react"
-import { Link, useSearchParams } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'း
+import { getVans } from "../../api";
 
 export default function Vans() {
     const [searchParams, setSearchParams] = useSearchParams();
@@ -8,9 +9,14 @@ export default function Vans() {
     const typeFilter = searchParams.get("type");
 
     React.useEffect(() => {
-        fetch("/api/vans")
-            .then(res => res.json())
-            .then(data => setVans(data.vans))
+        async function loadVans() {
+            setLoading(true)
+            const data = await getVans()
+            setVans(data)
+            
+        }
+        
+        loadVans()
     }, [])
 
     const displayedVans = typeFilter 
