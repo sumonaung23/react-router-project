53c28be9a900b9577216753cdf21963914e13645
sumonaung23 <sumonaung575@gmail.com>
2023-09-19 11:15:35 +0630

    imported NotFound.jsx and inserted Route inside the end of Layout route

--- index.jsx
+++ index.jsx
@@ -15,6 +15,7 @@ import HostVanPhotos from './pages/Host/HostVanPhotos';
 import HostVanInfo from './pages/Host/HostVanInfo';
 import HostVanPricing from './pages/Host/HostVanPricing';
 import HostLayout from './components/HostLayout';
+import NotFound from './pages/NotFound';
 
 import "./server"
 
@@ -39,6 +40,8 @@ function App() {
               <Route path='photos' element={<HostVanPhotos />} />
             </Route>
           </Route >
+
+          <Route path='*' element={<NotFound />} />
         </Route >
       </Routes>
     </BrowserRouter>
