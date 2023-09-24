394e3b1181743c48bd8636d228076d35e0963108
sumonaung23 <sumonaung575@gmail.com>
2023-09-24 11:22:08 +0630

    added useLoaderData and got rid of useEffect hook

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -1,24 +1,20 @@
 import React, { useEffect, useState } from 'react'
-import { Link, NavLink , useParams, Outlet } from 'react-router-dom';
+import { Link, NavLink , useParams, Outlet, useLoaderData } from 'react-router-dom';
+import { getHostVans } from '../../api';
+
+export function loader({ params }) {
+    return getHostVans(params.id)
+}
 
 const HostVanDetail = () => {
     const {id} = useParams();
-    const [currentVan, setCurrentVan] = useState(null);
+    //const [currentVan, setCurrentVan] = useState(null);
+    const currentVan = useLoaderData();
 
     const activeNav = {
         'font-weight': 'bold',
         'text-decoration': 'underline',
         'color': '#161616'
-    }
-
-    React.useEffect(() => {
-        fetch(`/api/host/vans/${id}`)
-            .then(res => res.json())
-            .then(data => setCurrentVan(data.vans));
-    }, []);
-
-    if(!currentVan) {
-        return <h1>Loading...</h1>
     }
 
     return (
