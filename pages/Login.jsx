bc64335bfd4f50acff7bada6e59679b539edbdf2
sumonaung23 <sumonaung575@gmail.com>
2023-09-24 12:18:18 +0630

    added useLoaderDate and loader function with request parameter

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,8 +1,13 @@
 import React from "react"
-import { useNavigate } from "react-router-dom"
+import { useLoaderData } from "react-router-dom"
+
+export function loader({ request }) {
+    return new URL(request.url).searchParams.get("message")
+}
 
 export default function Login() {
-    const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+    const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" });
+    const message = useLoaderData();
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -20,6 +25,7 @@ export default function Login() {
     return (
         <div className="login-container">
             <h1>Sign in to your account</h1>
+            { message && <h3 className="red">{message}</h3>}
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
