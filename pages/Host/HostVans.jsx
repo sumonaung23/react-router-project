3d7ee1ec53cfb5894ff0874296672f65b52543c4
sumonaung23 <sumonaung575@gmail.com>
2023-09-24 11:23:05 +0630

    added useLoaderData hook and ommited useEffect and useState hook

--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
+import { getHostVans } from '../../api'
+
+export function loader() {
+    return getHostVans()
+}
 
 const HostVans = () => {
-    const [vans, setVans] = React.useState([])
+    //const [vans, setVans] = React.useState([])
+    const vans = useLoaderData();
 
-    React.useEffect(() => {
+    /* React.useEffect(() => {
         fetch("/api/host/vans")
             .then(res => res.json())
             .then(data => setVans(data.vans))
-    }, [])
+    }, []) */
 
     const hostVansEls = vans.map(van => (
         <Link
@@ -30,16 +36,9 @@ const HostVans = () => {
         <section>
             <h1 className="host-vans-title">Your listed vans</h1>
             <div className="host-vans-list">
-                {
-                    vans.length > 0 ? (
-                        <section>
-                            {hostVansEls}
-                        </section>
-
-                    ) : (
-                            <h2>Loading...</h2>
-                        )
-                }
+                <section>
+                    {hostVansEls}
+                </section>
             </div>
         </section>
     )
