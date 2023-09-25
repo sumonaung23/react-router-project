a1c60180afb2fc0e2dfba95f9a5c9af61077c430
sumonaung23 <sumonaung575@gmail.com>
2023-09-25 23:45:20 +0630

    added useNavigate

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData, useNavigate } from "react-router-dom"
 import { loginUser } from "../api";
 
 export function loader({ request }) {
@@ -11,13 +11,14 @@ export default function Login() {
     const [status, setStatus] = React.useState("idle")
     const [error, setError] = React.useState(null)
     const message = useLoaderData();
+    const navigate = useNavigate();
 
     function handleSubmit(e) {
         e.preventDefault()
         setStatus("submitting")
         setError(null)
         loginUser(loginFormData)
-            .then(data => console.log(data))
+            .then(data => navigate("/host" , { replace: true }))
             .catch(err => setError(err))
             .finally(() => setStatus("idle"))
     }
