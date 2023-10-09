8187f56a35081a3bc31c91e0a02a539f6b5b0a32
sumonaung23 <sumonaung575@gmail.com>
2023-10-09 23:17:50 +0630

    modified isLogginedIn function

--- util.jsx
+++ util.jsx
@@ -1,7 +1,7 @@
 import { redirect } from "react-router-dom"
 
 export async function requireAuth() {
-    const isLoggedIn = true
+    const isLoggedIn = localStorage.getItem("loggedin")
     
     if (!isLoggedIn) {
         throw redirect("/login?message=you must log in first.")
